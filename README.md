3aba2445e1ffc83ae8c75e7430b4f6685ff59d63
Phan Thanh Giảng <giangb1609816@student.ctu.edu.vn>
2019-11-16 15:38:46 +0700

    Update all

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # V.A-Store
 
 Github: <a href="https://github.com/giangcse/V.A-Store">V.A-Store</a><br>
-Import Cơ sở dữ liệu quanlybanhang.sql trong thư mục database, nếu thiếu bảng thì import các cở sở dữ liệu khác cho đủ 8 bảng và ràng buộc như hình Capture.PNG
+Trang quản lý: /admin <br>
+Tài khoản: <strong>giangphan</strong>
+Mật khẩu: <strong>giang</strong>
+<i>Import Cơ sở dữ liệu quanlybanhang.sql trong thư mục database, nếu thiếu bảng thì import các cở sở dữ liệu khác cho đủ 8 bảng và ràng buộc như hình Capture.PNG</i>
 <img src="database/Capture.PNG" />
--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,20 @@
 <html lang="en">
+<?php
+  session_start();
+  include 'connect.php';
+  if (isset($_SESSION['MSNV'])){ 
+    $MSNV = $_SESSION['MSNV'];
+    $select_msnv = "SELECT ChucVu FROM nhanvien WHERE MSNV = '".$MSNV."'";
+    $result_msnv = mysqli_query($conn, $select_msnv);
+    if ($result_msnv->num_rows > 0)
+        while ($rows = mysqli_fetch_assoc($result_msnv)) {
+            if ($rows['ChucVu'] == "quanly"){
+              $MSNV = $_SESSION['MSNV'];
+              header('Location: dashboard.php');
+            }
+        }
+}
+?>
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
